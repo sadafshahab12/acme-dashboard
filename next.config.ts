d4aedc9e79a5d790b5798132844c704c59878ce3
Hassan3108huzaifa <huzaifa3108hassan@gmail.com>
2024-10-30 07:12:32 -0700

    change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
     experimental: {
         turbo: {
             rules: {
@@ -24,12 +23,5 @@ const nextConfig: NextConfig = {
         return config;
     },
 }
-=======
-  images: {
-    unoptimized: true,
-  },
-  /* other config options here */
-};
->>>>>>> 38b58474e0dfc7d688fad4ff1fff13d9423ebe95
 
 export default nextConfig
