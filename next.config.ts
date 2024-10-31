241628883b8556dca765f0b3cbf3fbf585ab8a8e
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2024-10-31 05:19:16 -0700

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-=======
 
->>>>>>> 8dd94f0c6b73b606869e00c773f1e49d4f0aeb84
 const nextConfig = {
   // Your Next.js configuration options here
 };
